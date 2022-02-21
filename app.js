be1268ebe2ceb7efa1b66607decdfdead8548b8d
Gonzalez Sanchez <stegonzalezsanchez@FVFZMYC6J1WK.tld>
2022-02-21 09:02:17 -0600

    finished user random numbers

--- app.js
+++ app.js
@@ -6,10 +6,32 @@ var comp = [];
 
 do{
     var ran = Math.floor(Math.random() * 10) + 1
+
     for(var i = 1; i <= ran; i++){
         comp.push(ran);
     }
     
     var totalComp = comp.length
+
     alert(`The new number is ${ran}.    The computer total is ${totalComp}.`)
+
 }while(totalComp <= 16);
+alert(`The compuer will stop at ${totalComp}.`)
+
+alert("Now it's the user's turn.")
+
+var user = [];
+
+do{
+    ran = Math.floor(Math.random() * 10) + 1
+
+    for(var i = 1; i <= ran; i++){
+        user.push(ran);
+    }
+
+    var totalUser = user.length
+
+    alert(`Your new number is ${ran}.    Your current total is ${totalComp}.`)
+
+    var x = prompt("Would you like another number? If you would like to stop, press 'n' to stop.")
+}while(x != "n")
