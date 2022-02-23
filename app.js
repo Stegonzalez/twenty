683b24613c3e21a9f64a4d5d6d089fa139bc56df
Gonzalez Sanchez <stegonzalezsanchez@FVFZMYC6J1WK.tld>
2022-02-23 08:46:11 -0600

    finished project 8

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 alert("Welcome to 20! This is a competition to see which player gets closer to th enumber 20. you are going to play agenst the computer. You will draw nimbers from 1 to 10. The computer will stop at 16. Pay attention to the screen to see what your total is, and if you need another number press'y'.");
 
-alert("Now... it's the computer's turn.")
+
+var userScore = [];
+var compScore = [];
+
+do{
 
 var comp = [];
+var user = [];
+
+alert("Now... it's the computer's turn.")
 
 do{
     var ran = Math.floor(Math.random() * 10) + 1
@@ -15,12 +22,11 @@ do{
 
     alert(`The new number is ${ran}.    The computer total is ${totalComp}.`)
 
-}while(totalComp <= 16);
+}while(totalComp < 16);
+
 alert(`The compuer will stop at ${totalComp}.`)
 
 alert("Now it's the user's turn.")
-
-var user = [];
 
 do{
     ran = Math.floor(Math.random() * 10) + 1
@@ -31,7 +37,65 @@ do{
 
     var totalUser = user.length
 
-    alert(`Your new number is ${ran}.    Your current total is ${totalComp}.`)
+    alert(`Your new number is ${ran}.    Your current total is ${totalUser}.`)
 
-    var x = prompt("Would you like another number? If you would like to stop, press 'n' to stop.")
+    if( totalUser > 19 ){
+
+        break;
+
+    }else{
+
+        var x = prompt("Would you like another number? If you would like to stop, press 'n' to stop.")
+
+    }
+    
 }while(x != "n")
+
+alert(`You stoped at ${totalUser}`);
+
+
+
+if (totalUser > 21){
+
+    alert(`You got ${totalUser}. Because you went over 20, the computer wins with ${totalComp}`)
+    compScore.push("w");
+
+}else if(totalComp > 21){
+
+    alert(`The computer got ${totalComp}. Because the computer is over 20, you win with ${totalUser}.`)
+    userScore.push("w");
+    
+}else if(totalUser > totalComp){
+
+    alert(`You win! You got ${totalUser}, and the computer got ${totalComp}.`)
+    userScore.push("w")
+
+}else if(totalUser < totalComp){
+
+    alert(`You Lost, You got ${totalUser}, and the computer got ${totalComp}.`)
+    compScore.push("w");
+
+}else if(totalUser == totalComp){
+
+    alert(`It's a tie! You both got ${totalUser}. No one gets a point.`);
+
+}
+alert(`The current score is User: ${userScore.length}, Computer: ${compScore.length}.`)
+
+var playAgain = prompt("Would you like to play again? Press 'y' to play again. Any other key will exit the program.")
+
+}while(playAgain == "y");
+
+if(userScore.length == compScore.length){
+
+    alert(`Thank you for playing. The final score is User: ${userScore.length}, Computer:${compScore.length}. It's a tie!`)
+
+}else if(userScore.length > compScore.length){
+
+    alert(`Thank you for playing. The final score is User: ${userScore.length}, Computer: ${compScore.length}. You won!`)
+
+}else if(userScore.length < compScore.length){
+
+    alert(`Thank you for playing. The final score is User: ${userScore.length}, Computer: ${compScore.length}. You lost!`)
+
+}
